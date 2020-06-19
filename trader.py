5662d059b951eb1054d062f0f1fb50c0d85e966e
skyegrey <dev.skyegrey@gmail.com>
2020-06-19 16:23:11 -0500

    + updated requirements + simplified trader + api_scratch file
    - Took away logging, way too many dependencies

--- trader.py
+++ trader.py
@@ -1,14 +1,10 @@
 from get_stock_info import get_stock_info
 
 from collections import deque
-from log_config import get_logger
 
-logger = get_logger('trader')
 
 frames = deque()
 
-# for i in range(10):
-#     frames.appendleft(get_stock_info('MSFT'))
-#     print(frames)
-
-
+for i in range(10):
+    frames.appendleft(get_stock_info('MSFT'))
+    print(frames)
