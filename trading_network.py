16e94507dfe22cf87cde75ce3ca6567fc20d5d1c
skyegrey <dev.skyegrey@gmail.com>
2020-06-19 20:58:35 -0500

    Changed value calculation to be more idiomatic Added repr

--- trading_network.py
+++ trading_network.py
@@ -3,5 +3,8 @@ class TradingNetwork:
     def __init__(self, output_module):
         self.output_module = output_module
 
-    def get_network_value(self):
+    def get_value(self):
         return self.output_module.calculate()
+
+    def __repr__(self):
+        return str(self.output_module)
