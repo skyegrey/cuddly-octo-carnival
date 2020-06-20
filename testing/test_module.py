2bcf4378146e18c7a04f1cff089792102ed75b68
skyegrey <dev.skyegrey@gmail.com>
2020-06-19 20:56:36 -0500

    Renamed module for more clarity (now network module) -- deleted test case to match

--- testing/test_module.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-from module import Module
-
-
-class TestModule(TestCase):
-
-    # Todo Setup testing when internet is avail
-    def test_calculate(self):
-        self.fail()
