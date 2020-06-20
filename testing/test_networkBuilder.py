d44551f931536a6dffe5864f40834c3c23f5fc7c
skyegrey <dev.skyegrey@gmail.com>
2020-06-19 20:55:13 -0500

    Added unit testing for network builder

---
+++ testing/test_networkBuilder.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestNetworkBuilder(TestCase):
+    def test_build_network(self):
+        self.fail()
+
+    def test_crossover_networks(self):
+        self.fail()
+
+    def test_mutate_network(self):
+        self.fail()
